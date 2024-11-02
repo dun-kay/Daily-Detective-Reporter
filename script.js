c5a6ba3c7ed106b2a8adcd24341ef41d38129a71
dun-kay <james@jumpship.media>
2024-11-02 09:10:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.clue-1-column').forEach(column => {
+document.querySelectorAll('.clue-1-column, .clue-2-column, .clue-3-column, .clue-4-column, .clue-5-column').forEach(column => {
   column.addEventListener('click', function() {
     // Remove the 'active' class from all 'l' divs in all columns
     document.querySelectorAll('.div-block-2.blank').forEach(lDiv => {

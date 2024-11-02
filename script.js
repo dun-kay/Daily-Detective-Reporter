9af06ee2aa35be57c8143beda7922fe1180c9a68
dun-kay <james@jumpship.media>
2024-11-02 09:15:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-document.querySelectorAll('.clue-1-column, .clue-2-column, .clue-3-column, .clue-4-column, .clue-5-column').forEach(column => {
-  column.addEventListener('click', function() {
-    // Remove the 'active' class from all 'l' divs in all columns
-    document.querySelectorAll('.div-block-2.blank').forEach(lDiv => {
-      lDiv.classList.remove('active');
-    });
 
-    // Add the 'active' class to 'l' divs within the clicked column
-    column.querySelectorAll('.div-block-2.blank').forEach(lDiv => {
-      lDiv.classList.add('active');
-    });
-  });
-});
